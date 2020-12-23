69174a87ed72c2417417a137cfa57dc755fb149a
Mikus <mikusmeikulis@gmail.com>
2020-12-23 13:53:17 +0200

    added h2  in home page

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
       <div className="row center-xs">
         <div className="col-xs-6">
           <h1>This is home page</h1>
+          <h2>First changes</h2>
         </div>
       </div>
     </div>
